e83997c3b9e7580049ac8f79e6df5510aca02ca1
xvw <xavier.vdw@gmail.com>
2016-06-06 02:53:58 +0200

    Add a title

--- source/MenuState.hx
+++ source/MenuState.hx
@@ -4,6 +4,7 @@ import flixel.FlxG;
 import flixel.FlxSprite;
 import flixel.FlxState;
 import flixel.text.FlxText;
+import flixel.util.FlxColor;
 import flixel.ui.FlxButton;
 import flixel.math.FlxMath;
 
@@ -12,6 +13,18 @@ class MenuState extends FlxState
 	override public function create():Void
 	{
 		super.create();
+		createTitle();
+	}
+
+	private function createTitle() {
+		var text = new FlxText();
+    	text.text = "A simple platformer";
+    	text.color = FlxColor.YELLOW;
+    	text.size = 32;
+    	text.alignment = FlxTextAlign.CENTER;
+    	text.setBorderStyle(FlxTextBorderStyle.SHADOW, FlxColor.RED, 4);
+    	text.screenCenter();
+    	add(text);
 	}
 
 	override public function update(elapsed:Float):Void
